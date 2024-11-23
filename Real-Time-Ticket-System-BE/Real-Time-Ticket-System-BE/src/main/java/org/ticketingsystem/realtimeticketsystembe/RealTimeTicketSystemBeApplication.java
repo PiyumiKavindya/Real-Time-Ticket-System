b0fde9b9868721684cfa20440b3b3ec8dd62d7c2
PiyumiWickramarathna <piyumiwi@lolctech.com>
2024-11-23 11:15:31 +0530

    first Commit with folders

---
+++ Real-Time-Ticket-System-BE/Real-Time-Ticket-System-BE/src/main/java/org/ticketingsystem/realtimeticketsystembe/RealTimeTicketSystemBeApplication.java
@@ -0,0 +1,13 @@
+package org.ticketingsystem.realtimeticketsystembe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicketSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicketSystemBeApplication.class, args);
+	}
+
+}
---
+++ Real-Time-Ticket-System-BE/Real-Time-Ticket-System-BE/src/test/java/org/ticketingsystem/realtimeticketsystembe/RealTimeTicketSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketingsystem.realtimeticketsystembe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicketSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
